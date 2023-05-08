94e29e8e3467a5747d7bc4f14b875813e0c8a3a7
Daniela Moreno <dani9292@hotmail.com>
2023-05-07 20:57:10 -0500

    Create a folder with store.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import '@/styles/app.css'
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
-     <App />    
+     <Provider store = {store}>
+     <App />  
+     </Provider>
+       
      </BrowserRouter>
  
   </React.StrictMode>,
