b37fa70c9497eecb7fff9186fbd2ff96031f7ee4
Daniela Moreno <dani9292@hotmail.com>
2023-05-11 12:48:54 -0500

    Fix flex

--- src/components/bookItem.jsx
+++ src/components/bookItem.jsx
@@ -61,37 +61,41 @@ const BookItem = ({
         </div>
       </div>
 
-      <div className="loading-status">
+      <div className="container-progress-circle">
+        <div className="loading-status">
 
-        <div className="spinner">
-          <div className="Oval-2" />
+          <div className="spinner">
+            <div className="Oval-2" />
+
+          </div>
+          <div className="completed-status">
+            <span className="-Percent-Complete">
+              17%
+            </span>
+            <span className="Completed Text-Style-2">
+              Completed
+            </span>
+          </div>
 
         </div>
-        <div className="completed-status">
-          <span className="-Percent-Complete">
-            17%
+        <div className="Line-3" />
+        <div className="book-status">
+          <span className="Current-Chapter Text-Style-7">
+            Current Chapter
           </span>
-          <span className="Completed Text-Style-2">
-            Completed
+          <span className="Current-Lesson Text-Style-4">
+            Chapter3:&ldquo;ALessonLearned&rdquo;
           </span>
+          <button type="button" className="btn-update-progress">
+            <span className="Update-progress">
+              UPDATE PROGRESS
+            </span>
+          </button>
+
         </div>
 
       </div>
-      <div className="Line-3" />
-      <div className="book-status">
-        <span className="Current-Chapter Text-Style-7">
-          Current Chapter
-        </span>
-        <span className="Current-Lesson Text-Style-4">
-          Chapter3:&ldquo;ALessonLearned&rdquo;
-        </span>
-        <button type="button" className="btn-update-progress">
-          <span className="Update-progress">
-            UPDATE PROGRESS
-          </span>
-        </button>
 
-      </div>
     </div>
   );
 };
